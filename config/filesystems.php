7e66c861dd56c9f8e1388b6b190b12166ddaef52
Ryan <ryan_zou@clbchain.com>
2018-11-17 14:14:14 +0800

    'laravelAdmin'

--- config/filesystems.php
+++ config/filesystems.php
@@ -63,6 +63,13 @@ return [
             'bucket' => env('AWS_BUCKET'),
         ],
 
+        'admin' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'visibility' => 'public',
+            'url' => env('APP_URL').'/storage',
+        ],
+
     ],
 
 ];
