93e48117d1753d2a53045318e9e4d5e08970004f
ryan <zouzhonghao@bookedu.com>
2019-02-27 18:04:16 +0800

    'redis'

--- app/Http/Controllers/Web/AppController.php
+++ app/Http/Controllers/Web/AppController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Web;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redis;
 
 class AppController extends Controller
 {
     public function getApp()
     {
+        Redis::set("name","ryan");
+        dd(111);
 //        return view('errors.503');
         return view('app');
     }
